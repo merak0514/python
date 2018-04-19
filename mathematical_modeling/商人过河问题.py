12f2885c1f54f1425a88047eb8263584b39b6a8d
merak <2692195809@qq.com>
2018-04-20 04:52:54 +0800

    优化：禁止直接原路返回

--- mathematical_modeling/商人过河问题.py
+++ mathematical_modeling/商人过河问题.py
@@ -10,7 +10,7 @@ class Boat(object):
 
     def __init__(self, merchants, minors, capacity):
         '''
-        分别为：原始岸边商人的数量、随从的数量；船的容纳量；禁止使用的策略
+        分别为：原始岸边商人的数量、随从的数量；船的容纳量；禁止使用的策略;上一次执行的策略
         '''
         # super(Boat, self).__init__()
         self.merchants = merchants
@@ -18,6 +18,7 @@ class Boat(object):
         self.capacity = capacity
         self.forbid_methods = []
         self.used_methods = []
+        self.last_operator = (0, 0)
 
         print('Initialized:', merchants, 'merchants and',
               minors, 'minors; capacity:', capacity)
@@ -77,7 +78,7 @@ class Boat(object):
 
     def forbidMethods(self, current, flag, method):
         self.forbid_methods.append((current, flag, method))
-        print("forbid_methods:", self.forbid_methods)
+        # print("forbid_methods:", self.forbid_methods)
 
     '''
     结合船的限制与禁止表得到的针对特定点的允许策略集合
@@ -90,7 +91,7 @@ class Boat(object):
             # print(current, method)
             if a == current and b == flag:
                 allow_actions_set.remove(method)
-        print('allow_actions_set aM:', allow_actions_set)
+        # print('allow_actions_set aM:', allow_actions_set)
         return allow_actions_set
 
     '''solution'''
@@ -102,29 +103,31 @@ class Boat(object):
         # print(current)
         count = 1  # 第n次渡船
         while current != (0, 0):
-            allow_actions_set = self.avaliableMethods(current, count % 2)
-
+            allow_actions_set = self.avaliableMethods(
+                current, count % 2)   # 允许采用的方法
             method = allow_actions_set[randint(
                 0, len(allow_actions_set) - 1)]  # 随机采用一种行动方式
-            # self.usedMethods(current, count % 2, method)
-            print(method)
-            temp = (current[0] + ((-1) ** count) * method[0],
-                    current[1] + ((-1) ** count) * method[1])
-            print(temp)
-            if temp in allowset:
-                current = temp
-                print('%d 个商人，%d 个随从，在第%d 次渡河的船上' %
-                      (method[0], method[1], count))
-                count += 1
-            else:
-                self.forbidMethods(current, count % 2, method)
+            if method != (self.last_operator[1], self.last_operator[0]):   # 禁止直接原路返回
+                # self.usedMethods(current, count % 2, method)
+                # print(method)
+                temp = (current[0] + ((-1) ** count) * method[0],
+                        current[1] + ((-1) ** count) * method[1])   # 尝试执行
+                # print(temp)
+                if temp in allowset:    # 若尝试执行后结果在允许的状态表中，则实际执行
+                    current = temp
+                    print('%d 个商人，%d 个随从，在第%d 次渡河的船上' %
+                          (method[0], method[1], count))
+                    count += 1
+                    self.last_operator = method
+                else:
+                    self.forbidMethods(current, count % 2, method)
 
 
 '''主方法'''
 
 
 def main():
-    boat = Boat(4, 4, 2)
+    boat = Boat(6, 6, 4)
     allowset = boat.allowSet()
     print("允许状态集合：")
     print(allowset)
