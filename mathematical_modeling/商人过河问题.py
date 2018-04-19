79739313eb95b177ce9b538366d1b0d640e793e3
merak <2692195809@qq.com>
2018-04-20 03:54:44 +0800

    减小随机数检索范围

--- mathematical_modeling/商人过河问题.py
+++ mathematical_modeling/商人过河问题.py
@@ -20,52 +20,66 @@ class Boat(object):
 
     '''allowset'''
 
-    def allowset(self):
+    def allowSet(self):
         allowset = []
         for merchants_here in range(self.merchants + 1):
             for minors_here in range(self.minors + 1):
                 # print([merchants_here, minors_here])
                 if merchants_here == 0 or merchants_here == self.merchants:
-                    allowset.append([merchants_here, minors_here])
+                    allowset.append((merchants_here, minors_here))
                 elif merchants_here >= minors_here and (self.merchants -
                                                         merchants_here) >= (self.minors - minors_here):
-                    allowset.append([merchants_here, minors_here])
+                    allowset.append((merchants_here, minors_here))
         return allowset
 
-    '''allowaction'''
+    '''allow_action'''
 
-    def allowaction(self):
-        allowaction = []
+    def allowAction(self):
+        allow_action = []
         for merchant_num in range(self.capacity + 1):
             for minor_num in range(self.capacity + 1 - merchant_num):
                 if minor_num + merchant_num != 0:
-                    allowaction.append([merchant_num, minor_num])
-        return allowaction
+                    allow_action.append((merchant_num, minor_num))
+        return allow_action
+
+    def usedMethod(self, current, flag, move):
+        self.used_method.append((current, flag, move))
+        print("used_method:", self.used_method)
+
+    def avaliableMethod(self, current, flag):
+        allow_action_set = self.allowAction()
+        # print('input current aM:', current)
+        for a, b, move in self.used_method:
+            # print(current, move)
+            if a == current and b == flag:
+                allow_action_set.remove(move)
+        print('allow_action_set aM:', allow_action_set)
+        return allow_action_set
 
     '''solution'''
 
-    def solve(self, allowset, allowactionset):
-        # method = 1  # 方法数
-        current = [self.merchants, self.minors]
+    def solve(self):
+        allowset = self.allowSet()
+        # print(allowset,'hhhhhhh')
+        current = (self.merchants, self.minors)
         # print(current)
         count = 1  # 第n次渡船
-        while current != [0, 0]:
-            move = allowactionset[randint(
-                0, len(allowactionset) - 1)]  # 随机采用一种行动方式
-            # print(move)
-            temp = [current[0] + ((-1) ** count) * move[0],
-                    current[1] + ((-1) ** count) * move[1]]
-            # print(temp)
+        while current != (0, 0):
+            allow_action_set = self.avaliableMethod(current, count % 2)
+
+            move = allow_action_set[randint(
+                0, len(allow_action_set) - 1)]  # 随机采用一种行动方式
+            # self.usedMethod(current, count % 2, move)
+            print(move)
+            temp = (current[0] + ((-1) ** count) * move[0],
+                    current[1] + ((-1) ** count) * move[1])
+            print(temp)
             if temp in allowset:
                 current = temp
                 print('%d 个商人，%d 个随从，在第%d 次渡河的船上' % (move[0], move[1], count))
                 count += 1
-
-    def usedMethod(self, current, move):
-        self.used_method.append([current, move])
-
-    def avaliableMethod(self):
-        pass
+            else:
+                self.usedMethod(current, count % 2, move)
 
 
 '''主方法'''
@@ -73,15 +87,15 @@ class Boat(object):
 
 def main():
     boat = Boat(3, 3, 2)
-    allowset = boat.allowset()
+    allowset = boat.allowSet()
     print("允许状态集合：")
     print(allowset)
 
-    allowactionset = boat.allowaction()
+    allow_action_set = boat.allowAction()
     print("允许决策集合：")
-    print(allowactionset)
+    print(allow_action_set)
 
-    boat.solve(allowset, allowactionset)
+    boat.solve()
 
 
 main()
