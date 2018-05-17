f435227d683fb6ec9029c2a8726e3893d5541de8
merak <2692195809@qq.com>
2018-05-17 09:52:06 +0800

    绘制标题module（其实只是想练习一下）

---
+++ project/2048/DrawTitle.py
@@ -0,0 +1,31 @@
+# !/merak/desktop/python/
+# @Time     : 8:37
+# @Author   : Merak
+# @File     : DrawTitle.py
+# @Software : PyCharm
+
+"""Draw Title"""
+print("import success")
+
+
+# def hello():
+#     print("hello")
+
+class DrawT:
+    def __init__(self, length, message, gameboard, score, high_score=0):
+        self.message = message
+        self.gameboard = gameboard
+        self.length = length
+        self.high_score = high_score
+        self.score = score
+
+    def draw_title(self):
+        self.gameboard.addstr(' '*int((self.length-4)/2) + '2048' + ' '*int((self.length-4)/2) + '\n')
+
+    def draw_message(self):
+        self.gameboard.addstr(' '*self.length-len(self.message) + self.message + '\n')
+
+    def draw_score(self):
+        self.gameboard.addstr('High Score: {:^6}'.format(self.high_score))
+        self.gameboard.addstr('    Score: {:^6}'.format(self.score))
+        self.gameboard.addstr('\n')
