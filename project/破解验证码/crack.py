d5d3254f3a93982d3e82584dc2429a94f31eebd0
merak <2692195809@qq.com>
2018-06-14 11:49:56 +0800

    把原始图片黑白化

--- project/破解验证码/crack.py
+++ project/破解验证码/crack.py
@@ -9,12 +9,20 @@ from PIL import Image
 import re
 grey_list = []
 front_color_set = []  # 主体部分使用颜色集
-background_color_set = []  # 背景部分颜色集
+background_color = 255  # 背景部分颜色集
 
 
 def to_grey(im):
     im.convert('P')
     sort_color(im)  # 得到有用的颜色集
+    new_im = Image.new("P", im.size,  255)
+    for i in range(im.size[0]):
+        for j in range(im.size[1]):
+            pix = im.getpixel((i, j))
+            if pix in front_color_set:
+                new_im.putpixel((i, j), 0)
+    return new_im
+
 
 
 def sort_color(im):
@@ -25,24 +33,21 @@ def sort_color(im):
     for i in range(256):
         value[i] = his[i]
     for j, k in sorted(value.items(), key=lambda a: a[1], reverse=True):
-        if last_color_freq == -1:
-            background_color_set.append(j)
-            last_color_freq = k
-            flag = 0
-        elif k >= 0.6 * last_color_freq and flag == 0:
-            background_color_set.append(j)
-            last_color_freq = k
-        elif k >= 0.5 * last_color_freq and flag == 1:
-            front_color_set.append(j)
-            last_color_freq = k
-        elif flag == 0:
-            front_color_set.append(j)
-            last_color_freq = k
+        if flag == 0:
+            background_color = j
             flag = 1
-        else:
-            break
-    print(background_color_set, front_color_set)
+            last_color_freq = -1
+        elif flag == 1:
+            if last_color_freq == -1:
+                front_color_set.append(j)
+                last_color_freq = k
+            elif k >= 0.5 * last_color_freq or last_color_freq == -1:
+                front_color_set.append(j)
+            else:
+                break
+    print(background_color, front_color_set)
 
 
 im = Image.open("python_captcha/captcha.gif")
-to_grey(im)
+im_new = to_grey(im)
+im_new.save('black.gif', 'GIF')
