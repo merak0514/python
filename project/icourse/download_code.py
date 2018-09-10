0d825446e5e265c833ea5e900154008e66a49dce
merak <2692195809@qq.com>
2018-09-10 13:05:44 +0800

    拿到目录了

--- project/icourse/download_code.py
+++ project/icourse/download_code.py
@@ -16,6 +16,7 @@ import Login
 import time
 import requests
 
+
 def batch_id():
     return round(time.time() * 1000)
 
@@ -30,28 +31,49 @@ class DownloadCode(object):
                       (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
             'origin': 'https://www.icourse163.org',
         }
+        self.terms = []
 
     def login(self):
+        """
+        调用Login进行登陆
+        :return: 0
+        """
         login = Login.Login()
         login.login(self.username, self.passwd)
         self.cookies = login.get_cookies()
+        return 0
 
     def get_term_info(self):
+        """
+        得到所有的学期及其对应的term_id
+        :return: 0
+        """
         if not self.cookies:
             print('请先登陆')
             return 1
         data = {
-            'callCount': 1,
+            'callCount': '1',
             'scriptSessionId': '${scriptSessionId}190',
             'c0-scriptName': 'PublishCourseBean',
             'c0-methodName': 'getTermsByTeacher',
             'c0-id': 0,
             'c0-param0': 'number:2',
-            'batch_id': batch_id(),
+            'batchId': batch_id()
         }
         host = 'https://www.icourse163.org/dwr/call/plaincall/PublishCourseBean.getTermsByTeacher.dwr'
         req = requests.post(host, headers=self.headers, data=data, cookies=self.cookies)
         print(req.text)
+        split = req.text.split('\n')
+
+        for line in split:
+            if line.startswith('s'):
+                term = {
+                    'term': re.findall('s([0-9]+)\.', line)[0],
+                    'term_id': re.findall('s[0-9]+\.termId=([0-9]+)', line)[0],
+                }
+                self.terms.append(term)
+        print(self.terms)
+        return 0
 
 
 if __name__ == '__main__':
