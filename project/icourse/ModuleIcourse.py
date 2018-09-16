2e86c26720657d4a7a3a1b91895bcbf3a63ba869
merak <2692195809@qq.com>
2018-09-16 16:21:16 +0800

    学期数据上传数据库

---
+++ project/icourse/ModuleIcourse.py
@@ -0,0 +1,31 @@
+# !/user/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 14:35
+# @Author   : Merak
+# @File     : ModuleIcourse.py
+# @Software : PyCharm
+import pymysql
+# pymysql.err.InternalError
+
+
+def upload_moc_data(data):
+    print('Connecting MySQL')
+    db = pymysql.connect('localhost', 'root', '', 'code_data_term1')
+    oj_set = data['oj_set']
+    test_set = data['test_set']
+    sql = "INSERT INTO `exercises`(`exName`, `exId`, `exType`, `avgScore`, `sbjTotalScore`," \
+          " `releaseTime`, `deadline`) VALUES(%s, %s, %s, %s, %s, %s, %s)"
+    cursor = db.cursor()
+    cursor.execute("set names 'utf8'")
+    for oj in oj_set:
+        cursor.execute(sql, (oj['name'], int(oj['id']), 2, float(oj['avgScore']),
+                             float(oj['sbjTotalScore']), oj['releaseTime'], oj['deadline']))
+    db.commit()
+    print('oj committed')
+    for test in test_set:
+        cursor.execute(sql, (test['name'], int(test['id']), 2, float(test['avgScore']),
+                             0, test['releaseTime'], test['deadline']))
+    print('test committed')
+    db.commit()
+    db.close()
+    print('Connecting closed')
--- project/icourse/download_code.py
+++ project/icourse/download_code.py
@@ -12,16 +12,15 @@ c++: ojLanguage = 2
 单元测试：type=2
 oj测试：type=7
 """
-import re
 import Login
 import time
 import requests
 import json
 import os
 import random_ua
-import AutoDownloader
 import GetData
 import random
+import ModuleIcourse
 
 
 def batch_id():
@@ -44,7 +43,7 @@ def new_folder(folder_path):
         try:
             os.makedirs(folder_path)
         except FileNotFoundError:
-            y = input('系统找不到指定的路径。' + folder_path)
+            input('系统找不到指定的路径。' + folder_path)
             return 1
         else:
             print('{} created'.format(folder_path))
@@ -58,6 +57,7 @@ class DownloadCode(object):
         self.ua = random_ua.UserAgent()
         self.username = account['username']
         self.passwd = account['passwd']
+        self.is_login = 0
         self.cookies = ''
         self.headers = {
             # 'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0',
@@ -83,6 +83,8 @@ class DownloadCode(object):
         login = Login.Login()
         login.login(self.username, self.passwd)
         self.cookies = login.get_cookies()
+        if self.cookies:
+            self.is_login = 1
         return 0
 
     def get_term_info(self):
@@ -90,13 +92,14 @@ class DownloadCode(object):
         得到所有的学期及其对应的term_id
         :return: 0
         """
-        if not self.cookies:
+        if not self.is_login:
             print('请先登陆')
             exit(1)
         data = GetData.get_term_info()
         host = 'https://www.icourse163.org/dwr/call/plaincall/PublishCourseBean.getTermsByTeacher.dwr'
         req = requests.post(host, headers=self.headers, data=data, cookies=self.cookies)
         self.terms = GetData.data_manage_term_info(req.text)
+        print(self.terms)
         req.close()
         return 0
 
@@ -107,6 +110,7 @@ class DownloadCode(object):
         :type term: int
         :return: req.text if success else 1
         """
+        self.get_term_info()  # 更新学期列表
         term_id = self.terms[term-1]['term_id'] if int(self.terms[term-1]['term']) == int(term) else -1
         if term_id == -1:
             print('terms没有按照顺序！')
@@ -118,20 +122,27 @@ class DownloadCode(object):
         print('编程题', self.oj_set)
         print('客观题', self.test_set)
         req.close()
+        ModuleIcourse.upload_moc_data({  # 上传数据库
+            'oj_set': self.oj_set[1: len(self.oj_set)],
+            'test_set': self.test_set[1: len(self.test_set)]
+        })
 
-    def begin_download(self):
+    def get_all_test_info(self, term):
         """
         根据练习的id找到对应练习的所有的作业
-        :return:
-        :rtype:
+        :param: term
+        :type: int
+        :return: oj_set
+        :rtype: list
         """
         host = 'http://www.icourse163.org/dwr/call/plaincall/MocScoreManagerBean.getStudentScoresByTestId.dwr'
-        term = 1
         page = 1
-        stu_per_page = 1000
+        stu_per_page = 20
         self.download_path += 'term' + str(term) + '/'
         new_folder(self.download_path)
         self.get_moc_data(term)  # 更新对应学期的课程列表
+
+        oj_set = []
         for i in range(1, len(self.oj_set)):
             oj = self.oj_set[i]
             folder_name = oj['name']
@@ -147,14 +158,35 @@ class DownloadCode(object):
             oj_stu_set = GetData.data_manage_oj_stu(req.text)
             print(oj_stu_set)
             print(len(oj_stu_set))
+            oj_set.append(oj_stu_set)
             req.close()
             time.sleep(random.randint(1, 10) / 5)
-            # break
+        return oj_set
 
-
+    def download(self, term, oj_stu_id):
+        """
+        :param term:
+        :type term: int
+        :param oj_stu_id:
+        :type oj_stu_id: complex
+        :return:
+        :rtype:
+        """
+        host = 'http://www.icourse163.org/dwr/call/plaincall/YocOJQuizBean.getOJPaperDto.dwr'
+        data = GetData.get_download_data(term, oj_stu_id)
+        req = requests.post(host, headers=self.headers, data=data, cookies=self.cookies)
+        # print(req.text)
 
     def auto_download(self):
-        pass
+        term = 1
+        oj_set = self.get_all_test_info(term)  # 得到该学期所有的oj测试
+        print(oj_set)
+        for oj_stu_set in oj_set:
+            for oj_stu in oj_stu_set:
+                self.download(term, oj_stu['id'])
+                break
+            break
+
 
 
 
@@ -164,6 +196,4 @@ if __name__ == '__main__':
     account_info = get_account()
     download = DownloadCode(account_info)
     download.login()
-    download.get_term_info()
-    time.sleep(1)
-    download.begin_download()
+    download.auto_download()
